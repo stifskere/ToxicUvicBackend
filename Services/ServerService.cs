1eb724e222760efb98b9266be784904a73b311e1
stifskere <stifskere@gmail.com>
2024-02-26 00:25:03 +0100

    fix: allow headers

--- Services/ServerService.cs
+++ Services/ServerService.cs
@@ -63,7 +63,10 @@ public class ServerService(HttpServer server, ILoggerFactory loggerFactory) : Ba
 
         // cors shit
         server.AddEndpoint(RequestMethodType.Options, new Regex(".+"), _ => new ResponseEntity(ResponseCodes.Ok));
-        server.AddGlobalMiddleware(_ => new NextMiddleWare().WithHeader("Access-Control-Allow-Origin", "*"));
+        server.AddGlobalMiddleware(_ => new NextMiddleWare()
+            .WithHeader("Access-Control-Allow-Origin", "*")
+            .WithHeader("Access-Control-Allow-Headers", "*")
+        );
         
         return Task.CompletedTask;
     }
